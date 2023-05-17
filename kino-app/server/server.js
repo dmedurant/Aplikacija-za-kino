4ee015bea0c53a04a874ef204bc900c372a17d12
dmedurant <dmedurant@veleri.hr>
2023-05-17 13:35:00 +0200

    testBuild_Commit_2

---
+++ kino-app/server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'your_host',
+  user: 'your_user',
+  password: 'your_password',
+  database: 'your_database',
+});
+
+// Connect to the MySQL database
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database');
+});
+
+// Define an API endpoint to fetch data from the database
+app.get('/api/data', (req, res) => {
+  const query = 'SELECT * FROM your_table';
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Error fetching data from database' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Start the server
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
