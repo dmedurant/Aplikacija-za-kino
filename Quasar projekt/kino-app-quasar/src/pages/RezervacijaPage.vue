d2ad714d042a04ed1874ae7db41185ef8ebbd2b9
unknown <nadia.alic3@gmail.com>
2023-06-12 17:24:50 +0200

    typo u kodu..

--- Quasar projekt/kino-app-quasar/src/pages/RezervacijaPage.vue
+++ Quasar projekt/kino-app-quasar/src/pages/RezervacijaPage.vue
@@ -143,9 +143,5 @@
     },
   }
   </script>
-  
   <style>
-  .bg-image {
-    background-image: url(https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v1016-c-08_1-ksh6mza3.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=f584d8501c27c5f649bc2cfce50705c0)
-  }
 </style>
